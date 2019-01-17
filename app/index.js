92c4129d1dcd356c29461c26cd04c1ca0520738e
Unknown <bruno@leaoteixeira.com.br>
2019-01-17 20:22:20 -0200

    Post and get transaction

--- app/index.js
+++ app/index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser')
 const Blockchain = require('../blockchain');
-const P2pServer = require('./p2p-server')
+const P2pServer = require('./p2p-server');
+const Wallet = require('../wallet');
+const TransactionPool = require('../wallet/transaction-pool');
+
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
 const app = express();
 const blockchain = new Blockchain();
+const wallet = new Wallet();
+const tp = new TransactionPool();
 const p2pServer = new P2pServer(blockchain);
 
 app.use(bodyParser.json());
@@ -23,5 +28,14 @@ app.post('/mine', (req,res) => {
     res.redirect('/blocks');
 });
 
+app.get('/transactions', (req,res) => {
+    res.json(tp.transactions);
+});
+
+app.post('/transact', (req,res) =>{
+    const { recipient, amount} = req.body;
+    const transaction = wallet.createTransaction(recipient, amount, tp);
+    res.redirect('/transactions');
+});
 app.listen(HTTP_PORT, () => console.log(`Listening in port ${HTTP_PORT}`));
 p2pServer.listen();
